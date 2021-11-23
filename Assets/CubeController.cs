f3ec2e19c2028c46c45af22a23b6e07c885fc9dc
chobochobo <h.masa.hero@gmail.com>
2021-11-23 12:00:24 +0900

    課題完了

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -5,28 +5,43 @@ using UnityEngine;
 public class CubeController : MonoBehaviour
 {
 
-    // キューブの移動速度
+    // ?????????
     private float speed = -12;
 
-    // 消滅位置
+    // ????
     private float deadLine = -10;
+
+    public AudioClip sound1;
+    AudioSource audioSource;
 
     // Use this for initialization
     void Start()
     {
 
+        //Component???
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // キューブを移動させる
+        // ??????????
         transform.Translate(this.speed * Time.deltaTime, 0, 0);
 
-        // 画面外に出たら破棄する
+        // ???????????
         if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
         }
+    }
+
+    //?????
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "block"|| other.gameObject.tag == "ground")
+        {
+            audioSource.PlayOneShot(sound1);
+        }
+
     }
 }
